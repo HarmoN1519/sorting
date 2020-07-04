46a57879d3e0a57e46b544ca3bd78b574d033b67
HarmoN1519 <patryk95wojtow@gmail.com>
2020-07-04 16:01:40 +0200

    first test commit

---
+++ src/main/java/pl/kszafran/sda/algo/exercises/selection_sorting/Main.java
@@ -0,0 +1,22 @@
+package pl.kszafran.sda.algo.exercises.selection_sorting;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 33, 17, 38, 92, 59, 4, 1, 9};
+
+        insertionSorting(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+    public static void insertionSorting (int[] numbers) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            
+        }
+        
+    }
+}
